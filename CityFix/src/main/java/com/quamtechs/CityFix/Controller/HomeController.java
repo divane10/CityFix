e2e777bffd48c659cc9d754b67713fb1f38a8d68
divane10 <tientcheudivane5@gmail.com>
2026-03-08 18:31:26 +0100

    Fix routing index html conflict

--- CityFix/src/main/java/com/quamtechs/CityFix/Controller/HomeController.java
+++ CityFix/src/main/java/com/quamtechs/CityFix/Controller/HomeController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/index.html";
-    }
+@GetMapping("/index.html")
+public String indexPage() {
+    return "redirect:/index.html";
+}
 }
