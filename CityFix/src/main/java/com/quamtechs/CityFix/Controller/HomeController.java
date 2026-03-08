c63bbc4eeeaec6387c367f5333bd2f429c899997
divane10 <tientcheudivane5@gmail.com>
2026-03-08 17:34:32 +0100

    Fix context path routing

--- CityFix/src/main/java/com/quamtechs/CityFix/Controller/HomeController.java
+++ CityFix/src/main/java/com/quamtechs/CityFix/Controller/HomeController.java
@@ -2,13 +2,11 @@ package com.quamtechs.CityFix.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "forward:/index.html";
+    public String home() {
+        return "redirect:/index.html";
     }
-
 }
