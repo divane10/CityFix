728f9ef0a3fd5a49be713b0d939d9b7b32333e6c
divane10 <tientcheudivane5@gmail.com>
2026-03-09 14:53:51 +0100

    fix controller routing

--- CityFix/src/main/java/com/quamtechs/CityFix/Controller/LocalisationController.java
+++ CityFix/src/main/java/com/quamtechs/CityFix/Controller/LocalisationController.java
@@ -3,12 +3,7 @@ package com.quamtechs.CityFix.Controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.quamtechs.CityFix.Entity.Localisation;
 import com.quamtechs.CityFix.Service.LocalisationService;
@@ -16,32 +11,29 @@ import com.quamtechs.CityFix.Service.LocalisationService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping
+@RequestMapping("/localisations")
 @RequiredArgsConstructor
 public class LocalisationController {
     
     private final LocalisationService localisationService;
-    // Récuperer toutes les localisation
 
+    // Récuperer toutes les localisations
     @GetMapping("/recupererLocalisation")
     public List<Localisation> getToutes() {
         return localisationService.getToutes();
     }
 
-    //Récuperer une localisation par son ID
-
+    // Récuperer une localisation par son ID
     @GetMapping("/{id}")
     public ResponseEntity<Localisation> getParId(@PathVariable Long id) {
         return localisationService.getParId(id)
                 .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build()); // si pas trouvé
+                .orElse(ResponseEntity.notFound().build());
     }
 
     // creer une nouvelle localisation 
-
     @PostMapping("/creer")
     public ResponseEntity<Localisation> creer(@RequestBody Localisation localisation) {
         return ResponseEntity.ok(localisationService.enregistrer(localisation));
     }
-
 }
